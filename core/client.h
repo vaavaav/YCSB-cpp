ba768ab8d40f580379c750735923a7e1fbae00a0
user Grupo de Sistemas Distribuidos <gsd@cloud137.cluster.lsd.di.uminho.pt>
2025-06-16 01:19:18 +0100

    Fix

--- core/client.h
+++ core/client.h
@@ -42,13 +42,13 @@ inline long ClientThread(std::chrono::seconds sleepafterload,
     long ops = 0;
 
     if (load) {
-       while(!wl->is_stop_requested()) {
-	    wl->DoInsert(*db);
-       }
+      while (!wl->is_stop_requested()) {
+        wl->DoInsert(*db);
+      }
     } else {
-	    while(!wl->is_stop_requested()) {
-		    wl->DoTransaction(*db);
-	    }
+      while (!wl->is_stop_requested()) {
+        wl->DoTransaction(*db);
+      }
     }
 
     if (cleanup_db) {
