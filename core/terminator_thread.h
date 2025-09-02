b3c2e32f8d0b4cc97bb522c54cd6efddd666891e
user Grupo de Sistemas Distribuidos <gsd@cloud137.cluster.lsd.di.uminho.pt>
2025-09-02 16:13:53 +0100

    Fixes

--- core/terminator_thread.h
+++ core/terminator_thread.h
@@ -1,11 +1,19 @@
 #pragma once
 #include "core_workload.h"
-#include <thread>
+#include <condition_variable>
 #include <future>
+#include <mutex>
+#include <thread>
 
 namespace ycsbc {
-    inline void TerminatorThread(std::chrono::seconds max_execution_time, ycsbc::CoreWorkload *wl) {
-        std::this_thread::sleep_for(max_execution_time);
-        wl->request_stop();
-    }
+inline void TerminatorThread(std::chrono::seconds max_execution_time,
+                             ycsbc::CoreWorkload *wl,
+                             std::condition_variable &cv, std::mutex &m) {
+  std::unique_lock<std::mutex> lock(m);
+  if (!cv.wait_for(lock, max_execution_time,
+                   [wl]() { return wl->is_stop_requested(); })) {
+    wl->request_stop();
+  }
 }
+
+} // namespace ycsbc
