c744e81cd0cdeb77bf550d50d2db6d52460d36ef
user Grupo de Sistemas Distribuidos <gsd@cloud137.cluster.lsd.di.uminho.pt>
2025-10-20 03:03:47 +0100

    Fix

--- holpaca-eval/RunYCSBOverhead.py
+++ holpaca-eval/RunYCSBOverhead.py
@@ -45,6 +45,7 @@ class Setup:
         name,
         executable,
         config,
+        with_controller=False,
         controller_exec=None,
         controller_args=None,
         status=None,
@@ -52,6 +53,7 @@ class Setup:
         self.name = name
         self.config = config
         self.executable = executable
+        self.with_controller = with_controller
         self.controller_exec = controller_exec
         self.controller_ip = config.get("cachelib.controller.address", None)
         self.controller_args = controller_args or ""
@@ -62,9 +64,87 @@ class Setup:
     def build_cmd(self):
         return f"{self.executable} -load -run -db cachelib-holpaca-overhead {f'-s {self.status}' if self.status else ''} {' '.join(f'-p {k}={v}' for k, v in self.config.items())}"
 
+    def run_controller(self, sifPath, binds=[], rehearse=False):
+        if not os.path.exists(sifPath):
+            raise FileNotFoundError(f"SIF file not found: {sifPath}")
+
+        if not os.path.exists(self.controller_exec):
+            raise ValueError(f"Controller executable not found: {self.controller_exec}")
+
+        # Store dool output in a local file
+        local_dool_output = "/tmp/dool.csv"
+        # Store YCSB output in a local file
+        local_ycsb_output = "/tmp/ycsb.txt"
+        # Prepare the output paths for dool
+        dool_output = os.path.join(self.out, "dool.csv")
+        # Prepare the output paths for YCSB
+        ycsb_output = os.path.join(self.out, "ycsb.txt")
+        # List commands to copy workload files to the local storage
+
+        # Prepare the command to run the SIF container of the client
+        client_inner_script = f""" "
+    CLIENT_IP=\\$(hostname -I | awk '{{print \\$1}}' | xargs)
+    IPS=\\"-p cachelib.controller.address=\\$CONTROLLER_IP:11110\\"
+    for i in \\$(seq 0 {self.threads - 1}); do
+      PORT=\\$((11111+i))
+      IPS+=\\" -p cachelib.holpaca.address.\\$i=\\$CLIENT_IP:\\$PORT\\"
+    done
+    singularity run --network host --bind '{','.join(binds)}' {sifPath} bash -c \\"
+        dool -cdlmnyt --output {local_dool_output} &
+        {self.build_cmd()} \\$IPS > {local_ycsb_output}
+        kill \\$(pgrep dool) 2>/dev/null || true
+    \\"
+    scancel \\"\\$CONTROLLER_JOB_ID\\" 2>/dev/null || true
+    cp {local_ycsb_output} {ycsb_output}
+    cp {local_dool_output} {dool_output}
+    " """
+
+        # Get the sbatch command for scheduling the client job
+        controller_sbatch = " ".join(
+            build_sbatch_cmd(
+                name=f"client-{self.name}",
+                cmd=client_inner_script,
+                stdout=f"/projects/F202400014TESTDEUCALION/pedro/YCSB-cpp/slurm-client-{self.name}.out",
+                stderr=f"/projects/F202400014TESTDEUCALION/pedro/YCSB-cpp/slurm-client-{self.name}.err",
+                export="CONTROLLER_IP=$CONTROLLER_IP,CONTROLLER_JOB_ID=$CONTROLLER_JOB_ID",
+            )
+        )
+
+        controller_dir = os.path.dirname(self.controller_exec)
+        executable_dir = os.path.dirname(self.executable)
+        # Prepare the command to run the SIF container of the controller
+        controller_inner_script = f"""
+    CONTROLLER_IP=$(hostname -I | awk '{{print $1}}' | xargs)
+    singularity run --network host --bind '{controller_dir},{executable_dir},{self.out},{','.join(binds)}' {sifPath} bash -c "
+        dool -cdlmnyt --output {self.out}/controller_dool.csv &
+        {self.controller_exec} $CONTROLLER_IP:11110 {self.controller_args} > {self.out}/controller.log 2>&1
+    " &
+    CONTROLLER_JOB_ID=$SLURM_JOB_ID
+    {controller_sbatch}
+    sleep infinity
+    """
+
+        sbatch_cmd = build_sbatch_cmd(
+            name=f"controller-{self.name}",
+            cmd=controller_inner_script,
+            stdout=f"/projects/F202400014TESTDEUCALION/pedro/YCSB-cpp/slurm-controller-{self.name}.out",
+            stderr=f"/projects/F202400014TESTDEUCALION/pedro/YCSB-cpp/slurm-controller-{self.name}.err",
+        )
+
+        if rehearse:
+            print(f"[REHEARSE] {' '.join(sbatch_cmd)}")
+            return
+
+        # Run the sbatch command to submit the controller job
+        subprocess.run(sbatch_cmd)
+
     def run(self, sifPath, binds=[], rehearse=False):
         if not os.path.exists(sifPath):
             raise FileNotFoundError(f"SIF file not found: {sifPath}")
+
+        if self.with_controller:
+            self.run_controller(sifPath, binds, rehearse)
+            return
 
         if not os.path.exists(self.out) and not rehearse:
             os.makedirs(self.out, exist_ok=True)
