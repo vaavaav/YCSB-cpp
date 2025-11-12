9a7926e7dd80ad2ea8669aa0c71e6d99023f1ab2
user Grupo de Sistemas Distribuidos <gsd@cloud137.cluster.lsd.di.uminho.pt>
2025-11-12 11:43:06 +0000

    Fix

--- holpaca-eval/RunYCSBOverhead.py
+++ holpaca-eval/RunYCSBOverhead.py
@@ -174,7 +174,7 @@ class Setup:
     done
 
     srun -n 1 singularity run --network host --bind '{controller_dir},{executable_dir},{self.out},{','.join(binds)}' {sifPath} bash -c "
-        pidstat -r -u -d -h 1 -- '{self.controller_exec} localhost:11110 {self.controller_args} > {self.out}/controller.log 2>&1' > /tmp/controller_pidstat.log 2>&1 
+        pidstat -r -u -d -h 1 -- bash -c '{self.controller_exec} localhost:11110 {self.controller_args} > {self.out}/controller.log 2>&1' > /tmp/controller_pidstat.log 2>&1 
     "
 
     srun -n 1 singularity run --network host --bind '/tmp,{','.join(binds)}' {sifPath} bash -c "
