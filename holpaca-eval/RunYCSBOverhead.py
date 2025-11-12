d838fb5c7741b441e75735407de491ccc46d1823
user Grupo de Sistemas Distribuidos <gsd@cloud137.cluster.lsd.di.uminho.pt>
2025-11-12 15:48:38 +0000

    Fix

--- holpaca-eval/RunYCSBOverhead.py
+++ holpaca-eval/RunYCSBOverhead.py
@@ -168,23 +168,25 @@ class Setup:
         # Prepare the command to run the SIF container of the client
         script = f""" 
         IPS="-p cachelib.controller.address=localhost:11110"
-    for i in $(seq 0 {self.threads - 1}); do
-      PORT=$((11111+i))
-      IPS+=" -p cachelib.holpaca.address.$i=localhost:$PORT"
-    done
+        for i in $(seq 0 {self.threads - 1}); do
+        PORT=$((11111+i))
+        IPS+=" -p cachelib.holpaca.address.$i=localhost:$PORT"
+        done
 
     srun -n 1 singularity run --network host --bind '{controller_dir},{executable_dir},{self.out},{','.join(binds)}' {sifPath} bash -c '
         {self.controller_exec} localhost:11110 {self.controller_args} > {self.out}/controller.log 2>&1 &
-        JOB_CONTROLLER=\$(pgrep -f '{os.path.basename(self.controller_exec)}' | head -n 1)
+        JOB_CONTROLLER=\$(pgrep -f '{os.path.basename(self.controller_exec)}' | tail -n 1)
         pidstat -u -p \$JOB_CONTROLLER 1 > {local_controller_pidstat_output} 2>&1
     ' &
 
     task1_pid=$!
 
+    sleep 1
+
     srun -n 1 singularity run --network host --bind '/tmp,{','.join(binds)}' {sifPath} bash -c "
         {self.build_cmd()} $IPS > {local_ycsb_output}
     " 
-    kill $task1_pid
+    kill -9 $task1_pid
 
     cp {local_controller_pidstat_output} {controller_pidstat_output}
     cp {local_ycsb_output} {ycsb_output}
