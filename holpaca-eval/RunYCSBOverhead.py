8932efc67b7d8ebf69cc12bb9fdf4ce998a11d93
user Grupo de Sistemas Distribuidos <gsd@cloud137.cluster.lsd.di.uminho.pt>
2025-11-12 15:31:16 +0000

    Fix

--- holpaca-eval/RunYCSBOverhead.py
+++ holpaca-eval/RunYCSBOverhead.py
@@ -173,11 +173,11 @@ class Setup:
       IPS+=" -p cachelib.holpaca.address.$i=localhost:$PORT"
     done
 
-    srun -n 1 singularity run --network host --bind '{controller_dir},{executable_dir},{self.out},{','.join(binds)}' {sifPath} bash -c "
+    srun -n 1 singularity run --network host --bind '{controller_dir},{executable_dir},{self.out},{','.join(binds)}' {sifPath} bash -c '
         {self.controller_exec} localhost:11110 {self.controller_args} > {self.out}/controller.log 2>&1 &
-        jobController=$(pgrep -f '{os.path.basename(self.controller_exec)}' | head -n 1)
-        pidstat -u -p $jobController 1 > {local_controller_pidstat_output} 2>&1
-    " &
+        JOB_CONTROLLER=\$(pgrep -f '{os.path.basename(self.controller_exec)}' | head -n 1)
+        pidstat -u -p \$JOB_CONTROLLER 1 > {local_controller_pidstat_output} 2>&1
+    ' &
 
     task1_pid=$!
 
